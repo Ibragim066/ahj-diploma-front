694eafcfa1bba3e5c86dbeef8cad8cefa736c7fa
Ibragim <gamemega.ao@gmail.com>
2024-05-04 16:05:12 +0300

    fix line-breaks style

--- src/js/components/poster/timer.js
+++ src/js/components/poster/timer.js
@@ -1,6 +1,6 @@
 export default class Timer {
-    constructor(sec, min) {
-      this.sec = sec;
-      this.min = min;
-    }
+  constructor(sec, min) {
+    this.sec = sec;
+    this.min = min;
   }
+}
